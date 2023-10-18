7044a49ddf90f7c57892d3a3b5c2feb6c8ff1929
Andressa Costa <emailandressacosta@gmail.com>
2023-10-18 16:56:38 -0300

    first commit

---
+++ src/main/java/dio/criandoapirest/CriandoApiRestApplication.java
@@ -0,0 +1,13 @@
+package dio.criandoapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriandoApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriandoApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/criandoapirest/CriandoApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.criandoapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriandoApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
